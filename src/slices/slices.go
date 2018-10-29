54a2efab7fe0d81dd86bfc76fb63543e21f5c2ec
Gustavo Peiretti <contacto@gustavopeiretti.com>
2018-10-29 17:30:18 -0300

    Slice

--- src/slices/slices.go
+++ src/slices/slices.go
@@ -15,13 +15,20 @@ func main() {
 	cities := []string{"buenos aires", "sao paulo", "new york"}
 	fmt.Println("Cities", cities)
 
-	// slice using make
+	// slice change value
+	cities[1] = "bogota"
+	fmt.Println("Cities with new value", cities)
+
+	// slice using make, len and cap = 5
 	m := make([]int, 5)
 	fmt.Printf("Slice m - lenght=%d capacity=%d values %v", len(m), cap(m), m)
 	fmt.Println()
 
-	mc := make([]int, 0, 5)
+	// slice using make, len = 0, cap = 5
+	mc := make([]int, 0, 10)
 	fmt.Printf("Slice mc - lenght=%d capacity=%d values %v", len(mc), cap(mc), mc)
 	fmt.Println()
 
+	mc = append(mc, 71)
+	fmt.Println(mc)
 }
